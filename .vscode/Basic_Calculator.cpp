ca1fbcaed84d5780bd3aeeeaf38d8651ba84437a
Subaraj <subaraj1961@gmail.com>
2024-03-05 09:19:53 +0530

    Basic_Calculator

---
+++ .vscode/Basic_Calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter the first num ";
+    cin >> x;
+    int y;
+    cout << "Enter the second num ";
+    cin >> y;
+    int sum= x+y;
+    cout << "Sum is " << sum;
+    return 0;
+
+}
